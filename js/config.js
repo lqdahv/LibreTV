48c7c8ebd5858236b9aea1fb6e7b147989d70947
lqdahv <l401955977@gmail.com>
2025-08-21 11:27:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,11 +27,6 @@ api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
 name: '电影天堂资源',
 detail: 'http://caiji.dyttzyapi.com',
 },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    }
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
